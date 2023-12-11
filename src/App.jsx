c70408295872f1cd7574fa29c176e49c80e806c6
hairunisa0929 <hairunisa.learning@gmail.com>
2023-12-11 16:25:09 +0700

    react context

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Layout from "./layout/Layout";
+import { CheckoutProvider } from "./context/CheckoutContext";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        {/* navigate for redirecting */}
-        {/* <Route path="/" element={<Navigate to="/home" />} /> */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
-    </Layout>
+    <CheckoutProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Layout>
+    </CheckoutProvider>
   );
 }
 
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,21 +1,89 @@
-import { useLocation, useParams } from "react-router";
-import { useSearchParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import useSWR from "swr";
+import { CheckoutContext } from "../context/CheckoutContext";
+import { toRupiah } from "../utils/formatter";
+
+const fetcher = (url) => axios.get(url).then((response) => response.data);
 
 function Detail() {
-  // useParams
-  // const { id } = useParams();
-  // console.log(id);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { setDataCheckout } = useContext(CheckoutContext);
 
-  // get data with useSearchParams (params showed in url)
-  // const [searchParams] = useSearchParams();
-  // console.log(searchParams.get("name"));
+  const [qty, setQty] = useState(1);
 
-  // get data with useLocation (state not showed in url)
-  // const location = useLocation();
-  // console.log("location", location.state.name);
+  const { data, isLoading } = useSWR(
+    `http://localhost:3000/pokemon/${id}`,
+    fetcher
+  );
+
+  if (isLoading) return <BeatLoader color="#38BDF8" />;
+
+  const incrementQty = () => setQty(qty + 1);
+  const decrementQty = () => {
+    if (qty > 1) {
+      setQty(qty - 1);
+    }
+  };
+
+  const onClickBuyNow = () => {
+    setDataCheckout({
+      ...data,
+      qty,
+    });
+    navigate("/checkout");
+  };
 
   return (
-    <div>Detail</div>
+    <section>
+      <div className="flex">
+        <img src={data.img} alt={data.name} className="w-1/2" />
+
+        <div className="flex flex-col gap-4">
+          <h1 className="font-bold text-2xl">{data.name}</h1>
+          <h1 className="font-bold text-xl">{toRupiah(data.price)}</h1>
+          <div>
+            <span>Type</span>
+            <span className="bg-sky-400 text-white py-[5px] px-[8px] ml-2 rounded-lg">
+              {data.type}
+            </span>
+          </div>
+          <p>{data.description}</p>
+          <span>Quantity</span>
+          <div className="flex flex-row h-10 rounded-lg bg-transparent mt-1">
+            <button
+              className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-10 rounded-l cursor-pointer outline-none text-2xl font-thin"
+              onClick={decrementQty}
+            >
+              −
+            </button>
+            <input
+              type="number"
+              className="outline-none focus:outline-none text-center bg-gray-300 w-20 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
+              name="qty"
+              value={qty}
+              disabled
+            />
+            <button
+              className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-10 rounded-r cursor-pointer text-2xl font-thin"
+              onClick={incrementQty}
+            >
+              +
+            </button>
+          </div>
+
+          <button
+            className="rounded-lg bg-sky-400 p-2 text-white w-fit"
+            onClick={onClickBuyNow}
+          >
+            Buy Now
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
