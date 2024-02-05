87c9d99828cfc0a3d75df0ef55ab821baef3665b
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-02-05 14:15:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import GuestGreeting from "./components/FunctionalComponent";
 
 function App() {
   const name = "Forger";
+  const image = "https://www.purina.co.uk/sites/default/files/2023-03/Hero%20Pedigree%20Cats.jpg";
 
   const onClickButton = (role) => alert(`Logged in as ${role}`);
 
   return (
     <>
       {/* <UserGreeting userName={name} /> */}
-      <GuestGreeting userName={name} getRole={onClickButton} />
+      <GuestGreeting userName={name} image={image} />
     </>
   );
 }
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -3,18 +3,20 @@ import Button from "./Button";
 
 function GuestGreeting(props) {
   const role = "user";
+  // const { userName, image } = props;
 
   return (
     <div>
       <h1>Hello {props.userName}</h1>
+      <img src={props.image} alt={props.userName} width={500} />
       {/* <button onClick={() => props.getRole(role)}>Get Role</button> */}
 
-      <Button
+      {/* <Button
         onClick={() => props.getRole(role)}
         style={{ backgroundColor: "blue", color: "white" }}
       >
         Get Role
-      </Button>
+      </Button> */}
     </div>
   );
 }
