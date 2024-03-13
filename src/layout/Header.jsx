a41b7dd2626f6c98b74c34ac65f207c2cd76d693
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-03-13 13:12:19 +0700

    delete unnecessary code

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { BiCartAlt } from "react-icons/bi";
